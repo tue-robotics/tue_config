d8f4d364d44c40c7d22c65dadb124458dae07eba
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2015-08-03 15:06:08 +0200

    Much cleaner python implementation

--- src/tue_config/configuration.py
+++ src/tue_config/configuration.py
@@ -1,82 +1,41 @@
 class Configuration:
 
-    def __init__(self):
-        self.__dict__["_values"] = {}
-        self.__dict__["_groups"] = {}
-        self.__dict__["_array"] = None
-        self.__dict__["_dict"] = {}
-
-    def __iadd__(self, value):
-        a = self.__dict__["_array"]
-        if not a:
-            a = []
-        a += value
-        self.__dict__["_array"] = a
-        return self
+    def __init__(self, d=None):
+        self.__dict__["_d"] = d if d != None else {}
 
     def __getattr__(self, key):
         if key[0] == '_':
-            return object.__getattr__(self, key)
+            return object.__getattr__(self, key)        
 
-        if key in self.__dict__["_values"]:
-            raise AttributeError, "Cannot create group '{}': is already a value name".format(key)
+        d = self.__dict__["_d"]
+        if key in d:
+            if isinstance(d[key], dict):
+                return Configuration(d[key])
+            else:
+                return d[key]
 
-        groups = self.__dict__["_groups"]
-        if not key in groups:
-            groups[key] = Configuration()
-        return groups[key]
+        d[key] = {}
+        return Configuration(d[key])
 
-    def __setattr__(self, name, value):
-        if isinstance(value, Configuration):
-            if name in self.__dict__["_values"]:
-                raise AttributeError, "Cannot create group '{}': is already a value name".format(name)
-            self.__dict__["_groups"][name] = value
-            return
+    def __setattr__(self, key, value):
+        if isinstance(value, Configuration):  
+            self.__dict__["_d"][key] = value.__dict__["_d"]
         elif isinstance(value, list):
-            if value and isinstance(value[0], Configuration):
-                g = self.__getattr__(name)
-                g.__dict__["_array"] = value
-                return
+            # Replace all occurances of Configuration with the dict equivalent
+            self.__dict__["_d"][key] = [ i.__dict__["_d"] if isinstance(i, Configuration) else i for i in value]
+        elif isinstance(value, Configuration):
+            self.__dict__["_d"][key] = value.__dict__["_d"]
+        else:
+            self.__dict__["_d"][key] = value
 
-        if name in self.__dict__["_groups"]:
-            raise AttributeError, "Cannot assign '{}': is already a group name".format(name)
-        self.__dict__["_values"][name] = value
+    def _dict(self):
+        return self.__dict__["_d"]
 
-    def _toDict(self):
-        d = self.__dict__["_dict"]
+    def __str__(self):
+        return str(self._dict())
 
-        a = self.__dict__["_array"]
-        if a:
-            return [i._toDict() for i in a]
-
-        d.update(self.__dict__["_values"].copy())
-
-        for (k, g) in self.__dict__["_groups"].iteritems():
-            d[k] = g._toDict()
-
-        return d
-
-# ----------------------------------------------------------------------------------------------------
-
-#def from_dict(d):
-#    print "BLAA: {}".format(d)
-#
-#    cfg = Configuration()
-#    for (k, v) in d.iteritems():
-#        if isinstance(v, dict):
-#            cfg.__setattr__(k, from_dict(v))
-#        elif isinstance(v, list):
-#            if v and isinstance(v[0], dict):
-#                for i in v:
-#                    g = cfg.__getattr__(k)     
-#                    #cfg.__dict__["_array"] += [from_dict(i)]    
-#                    g.__iadd__([from_dict(i)])
-#            else:
-#                cfg.__setattr__(k, v)
-#        else:
-#            cfg.__setattr__(k, v)
-#
-#    return cfg
+    def __repr__(self):
+        return self._dict()
 
 # ----------------------------------------------------------------------------------------------------
 
@@ -98,12 +57,12 @@ def from_file(filename, relative=True):
             raise AttributeError, output
 
         import yaml
-        cfg.__dict__["_dict"] = yaml.load(output)
+        cfg.__dict__["_d"] = yaml.load(output)
     elif full_filename.endswith(".yaml") or full_filename.endswith(".yml") or full_filename.endswith(".json"):
         # since JSON is a sub-set of YAML, we can always use the YAML parser
         import yaml
         with open(full_filename, 'r') as f:
-            cfg.__dict__["_dict"] = yaml.load(f)
+            cfg.__dict__["_d"] = yaml.load(f)
     else:
         raise AttributeError, "Unknown config file format: {}".format(filename)
 
@@ -113,7 +72,6 @@ def from_file(filename, relative=True):
 
 def write_yaml(config):
     import yaml
-    print yaml.dump(config._toDict(), default_flow_style=True, width=1e9)
-
+    print yaml.dump(config._dict(), default_flow_style=True, width=1e9)
 
 
