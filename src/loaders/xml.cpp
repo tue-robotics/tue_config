7525b666de0f5a221c8b58dd0ee140b3d0c31044
Janno Lunenburg <jannolunenburg@gmail.com>
2018-10-31 21:59:48 +0100

    fix indentation

--- src/loaders/xml.cpp
+++ src/loaders/xml.cpp
@@ -16,31 +16,31 @@ namespace config
 
 bool loadFromXMLStream(std::istream& stream, ReaderWriter& config)
 {
-  throw tue::config::ParseException("loadFromXMLStream is not yet implemented");
-  return false;
+    throw tue::config::ParseException("loadFromXMLStream is not yet implemented");
+    return false;
 }
 
 // ----------------------------------------------------------------------------------------------------
 
 bool loadFromXMLString(const std::string& string, ReaderWriter& config)
 {
-  throw tue::config::ParseException("loadFromXMLString is not yet implemented");
-  return false;
+    throw tue::config::ParseException("loadFromXMLString is not yet implemented");
+    return false;
 }
 
 // ----------------------------------------------------------------------------------------------------
 
 bool loadFromXMLText(TiXmlElement& element, ReaderWriter& config)
 {
-  std::string key(element.Value());
-  if (element.GetText() == NULL)
-  {
-    std::cout << "Skipping " << element.Value() << std::endl;
+    std::string key(element.Value());
+    if (element.GetText() == NULL)
+    {
+        std::cout << "Skipping " << element.Value() << std::endl;
+        return true;
+    }
+    std::string value(element.GetText());
+    config.setValue(key, value);
     return true;
-  }
-  std::string value(element.GetText());
-  config.setValue(key, value);
-  return true;
 }
 
 // ----------------------------------------------------------------------------------------------------
@@ -48,48 +48,48 @@ bool loadFromXMLText(TiXmlElement& element, ReaderWriter& config)
 // ToDo: make TiXmlElement const
 bool loadFromXMLElement(TiXmlElement& element, ReaderWriter& config)
 {
-  if (element.FirstChildElement() == NULL)
-  {
-    std::cout << "Parsing text " << std::endl;
-    return loadFromXMLText(element, config);
-  }
-  else
-  {
-    std::cout << "Parsing element " << std::endl;
-
-    // Start a new array with the Value of the current element as key
-    std::string element_name = element.Value();
-    config.writeArray(element_name);
-
-    // Iterate through attributes
-    // ToDo: this does not work if this element does not contain children (we don't end up here)
-    for (const TiXmlAttribute* attribute = element.FirstAttribute(); attribute != NULL; attribute = attribute->Next())
+    if (element.FirstChildElement() == NULL)
     {
-        config.addArrayItem();
-        config.setValue(attribute->Name(), attribute->Value());
-        config.endArrayItem();
+        std::cout << "Parsing text " << std::endl;
+        return loadFromXMLText(element, config);
+    }
+    else
+    {
+        std::cout << "Parsing element " << std::endl;
+
+        // Start a new array with the Value of the current element as key
+        std::string element_name = element.Value();
+        config.writeArray(element_name);
+
+        // Iterate through attributes
+        // ToDo: this does not work if this element does not contain children (we don't end up here)
+        for (const TiXmlAttribute* attribute = element.FirstAttribute(); attribute != NULL; attribute = attribute->Next())
+        {
+            config.addArrayItem();
+            config.setValue(attribute->Name(), attribute->Value());
+            config.endArrayItem();
+        }
+
+        // Recurse through models
+        for(TiXmlElement* e = element.FirstChildElement(); e != NULL; e = e->NextSiblingElement())
+        {
+            std::string candidate_name = e->Value();
+            config.addArrayItem();
+
+            if (!loadFromXMLElement(*e, config))
+            {
+                std::stringstream error_msg;
+                error_msg << "Error parsing " << e->Value();
+                config.addError(error_msg.str());
+                std::cout << error_msg.str() << std::endl;
+                return false;
+            }
+            config.endArrayItem();
+        }
+        config.endArray();
     }
 
-    // Recurse through models
-    for(TiXmlElement* e = element.FirstChildElement(); e != NULL; e = e->NextSiblingElement())
-    {
-      std::string candidate_name = e->Value();
-      config.addArrayItem();
-
-      if (!loadFromXMLElement(*e, config))
-      {
-        std::stringstream error_msg;
-        error_msg << "Error parsing " << e->Value();
-        config.addError(error_msg.str());
-        std::cout << error_msg.str() << std::endl;
-        return false;
-      }
-      config.endArrayItem();
-    }
-    config.endArray();
-  }
-
-  return true;
+    return true;
 
 }
 
@@ -97,41 +97,41 @@ bool loadFromXMLElement(TiXmlElement& element, ReaderWriter& config)
 
 bool loadFromXMLFile(const std::string& filename, ReaderWriter& config)
 {
-  config.setSource(filename);
+    config.setSource(filename);
 
-//  std::ifstream fin(filename.c_str());
-//  if (fin.fail())
-//  {
-//      config.addError("No such file: '" + filename + "'.");
-//      return false;
-//  }
+    //  std::ifstream fin(filename.c_str());
+    //  if (fin.fail())
+    //  {
+    //      config.addError("No such file: '" + filename + "'.");
+    //      return false;
+    //  }
 
-//  if (!loadFromXMLStream(fin, config))
-//      return false;
+    //  if (!loadFromXMLStream(fin, config))
+    //      return false;
 
-  // Load the file
-  TiXmlDocument doc(filename);
-  doc.LoadFile();
-  if (doc.Error())
-  {
-    std::stringstream error_msg;
-    error_msg << "Error loading " << filename << ": ";
-    error_msg << doc.ErrorDesc() << " at row " << doc.ErrorRow() << ", col " << doc.ErrorCol();
-    config.addError(error_msg.str());
-    return false;
-  }
+    // Load the file
+    TiXmlDocument doc(filename);
+    doc.LoadFile();
+    if (doc.Error())
+    {
+        std::stringstream error_msg;
+        error_msg << "Error loading " << filename << ": ";
+        error_msg << doc.ErrorDesc() << " at row " << doc.ErrorRow() << ", col " << doc.ErrorCol();
+        config.addError(error_msg.str());
+        return false;
+    }
 
-  // Get the root
-  // ToDo: only sdf?
-  TiXmlElement* root = doc.FirstChildElement();
+    // Get the root
+    // ToDo: only sdf?
+    TiXmlElement* root = doc.FirstChildElement();
 
-  if (root->NextSibling() != NULL)
-  {
-    throw tue::config::ParseException("A valid XML file should only contain one root element");
-  }
+    if (root->NextSibling() != NULL)
+    {
+        throw tue::config::ParseException("A valid XML file should only contain one root element");
+    }
 
-//  std::string key("model");
-  return loadFromXMLElement(*root, config);
+    //  std::string key("model");
+    return loadFromXMLElement(*root, config);
 
 }
 
