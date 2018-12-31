5dd9933dfa725615ed33911e5fa2338bd97cd745
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2018-12-31 17:43:50 +0100

    change test to model as array

--- test/sdf_gtest.cpp
+++ test/sdf_gtest.cpp
@@ -16,19 +16,12 @@ TEST(SDF, LoadSDF)
     EXPECT_TRUE(config.readGroup("sdf"));
 }
 
-TEST(SDF, readGroup)
-{
-    EXPECT_FALSE(config.readArray("model"));
-    EXPECT_TRUE(config.readGroup("model"));
-    EXPECT_TRUE(config.endGroup());
-}
-
 TEST(SDF, String)
 {
     std::string name;
     double nameD = 0.;
     int nameI = 0;
-    config.readGroup("model");
+    config.readArray("model");
     EXPECT_FALSE(config.value("name", nameD));
     EXPECT_FALSE(config.value("name", nameI));
     EXPECT_TRUE(config.value("name", name));
@@ -49,14 +42,17 @@ TEST(SDF, Double)
 
 TEST(SDF, readArray)
 {
-    config.readGroup("model");
-    EXPECT_FALSE(config.readGroup("link"));
-    EXPECT_TRUE(config.readArray("link"));
+    EXPECT_FALSE(config.readGroup("model"));
+    EXPECT_TRUE(config.readArray("model"));
     EXPECT_TRUE(config.nextArrayItem());
+    EXPECT_TRUE(config.endArray());
 }
+
 
 TEST(SDF, Int)
 {
+    config.readArray("link");
+    config.nextArrayItem();
     int linkname = 0;
     double linknameD = 0.;
     std::string linknameS = "";
