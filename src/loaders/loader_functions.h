b755cd6102effdad8018ea36c3edf4320bc4c0f1
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2019-05-18 14:31:27 +0200

    Add docstring to strToBool

--- src/loaders/loader_functions.h
+++ src/loaders/loader_functions.h
@@ -8,6 +8,12 @@ namespace tue
 namespace config
 {
 
+/**
+ * @brief strToBool Converts a string to a boolean of matches one of the following: y, n, yes, no, true, false, on, off
+ * @param s string to convert
+ * @param rhs bool to set to true or false
+ * @return indicates succes, rhs should only be used if return is true.
+ */
 bool strToBool(std::string s, bool& rhs);
 
 }
